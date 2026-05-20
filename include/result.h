5a03861eca8bf69c6ebeca619f6addbf819a9d39
uhlickmate <ondrej.hulinsky.007@gmail.com>
2026-05-20 20:58:54 +0200

    feat: finish basic res

--- include/result.h
+++ include/result.h
@@ -6,20 +6,32 @@
 #include <stdbool.h>
 
 #define Result(T, E) struct { \
-	union { T ok; E err; } value; \
+	union { T ok; E err_t; } value; \
 	bool is_ok; \
-	T (*unwrap)(void* self); \
-} \
+}
 
-#define unwrap ({ \
-    if (!res.is_ok) { \
-        fprintf(stderr, "PANIC: Attempted unwrap on `Err` result \n"); \
+#define unwrap_res(res) ({ \
+    __typeof__(res) _res = (res); \
+    if (!_res.is_ok) { \
+        fprintf(stderr, "PANIC: Attempted unwrap on `Err` result\n"); \
         exit(1); \
     } \
-    res.value.ok; \
+    _res.value.ok; \
 })
 
+#define expect_res(res, msg) ({ \
+    __typeof__(res) _res = (res); \
+    if (!_res.is_ok) { \
+        fprintf(stderr, "PANIC: %s\n", (msg)); \
+        exit(1); \
+    } \
+    _res.value.ok; \
+})
+
+#define is_valid(res) ({ (res).is_ok; })
+#define is_err(res) ({ !(res).is_ok; })
+
 #define Ok(val) { .value.ok = (val), .is_ok = true }
-#define Err(err) { .value.err = (err), .is_ok = false }
+#define Err(err) { .value.err_t = (err), .is_ok = false }
 
 #endif
