0cfe32aea1f7cf49fb29b184dfb976c17b2c1d34
uhlickmate <ondrej.hulinsky.007@gmail.com>
2026-05-20 22:17:32 +0200

    feat: start assert_eq

--- include/macros.h
+++ include/macros.h
@@ -3,15 +3,9 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
-//
-// Output
-//
-// println, print, errors & such
-//
-
-// Both guess type of `x` & choose a format sign
-#define _print_fmt(x) _Generic((x), \
+#define _fmt_sqc(x) _Generic((x), \
     bool: "%s ", \
     char: "%c ", \
     int: "%d ", \
@@ -24,12 +18,19 @@
     const char*: "%s ", \
     default: "%p " \
 )
-#define _print_val(x) _Generic((x), \
+#define _fmt_other(x) _Generic((x), \
     bool: (x) ? "true" : "false", \
     default: (x) \
 )
 
-#define _print_single(x) printf(_print_fmt(x), _print_val(x))
+
+
+
+// ###########
+// Output
+// ###########
+
+#define _print_single(x) printf(_fmt_sqc(x), _fmt_other(x))
 
 #define _GET_MACRO(_1, _2, _3, _4, _5, NAME, ...) NAME // Counts arguments
 
@@ -39,6 +40,9 @@
 #define _print_3(a, b, c)    _print_single(a); _print_single(b); _print_single(c)
 #define _print_4(a, b, c, d) _print_single(a); _print_single(b); _print_single(c); _print_single(d)
 #define _print_5(a, b, c, d, e) _print_single(a); _print_single(b); _print_single(c); _print_single(d); _print_single(e)
+
+
+// Access functions
 
 #define print(...) do { \
     _GET_MACRO(__VA_ARGS__, _print_5, _print_4, _print_3, _print_2, _print_1)(__VA_ARGS__); \
@@ -53,12 +57,23 @@
 
 
 
+// ###########
+// Comparison
+// ###########
+
+#define _cmp(a, b) _Generic((a) \
+    char*: strcmp((a), (b)) == 0, \
+    default: (a) == (b) \
+)
 
 
+// Access functions
+#define assert_eq(a, b) do { \
+	if (!_cmp(a, b)) {
+		
+	}
+}
 
 
-// #define assert_eq(a, b) do { \
-
-// }
 
 #endif
