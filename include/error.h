4a3f64d83442255a9fc897142bff84bc171add7b
uhlickmate <ondrej.hulinsky.007@gmail.com>
2026-05-20 20:58:06 +0200

    feat: add error messages

--- include/error.h
+++ include/error.h
@@ -3,11 +3,24 @@
 
 typedef enum {
 	ERR_NONE,
-} ErrType;
+	ERR_GENERIC
+} Error;
 
-typedef struct {
-	ErrType type;
-	const char *msg;
-} Err;
+#define ERROR_LIST(X) \
+	X(ERR_NONE, "No errors encountered") \
+	X(ERR_GENERIC, "Generic error encountered") 
+
+static inline const char* error_to_string(Error err) {
+	static const char* msg_table[] = {
+		#define AS_STRING_ARRAY(name, string) [name] = string,
+		ERROR_LIST(AS_STRING_ARRAY)
+		#undef AS_STRING_ARRAY
+	};
+	
+	if (err < 0 || err >= (sizeof(msg_table) / sizeof(msg_table[0]))) {
+		return "Unknown or unmapped crust execution fault";
+	}
+	return msg_table[err];
+}
 
 #endif
