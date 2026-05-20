eba4b017d0a8fd15fda5791fef4693924d57a219
uhlickmate <ondrej.hulinsky.007@gmail.com>
2026-05-20 20:59:03 +0200

    test: test

--- main.c
+++ main.c
@@ -4,6 +4,12 @@
 
 
 int main() {
-	Result(int, Err) x = Ok(1);
-	printf("%d", unwrap(x));
+	Result(float, Error) res_ok = Ok(7.5);
+	Result(int, Error) res_err = Err(ERR_NONE);
+	Option(int) opt_some = Some(15);
+	Option(bool) opt_none = None;
+
+	println("res_ok has a value of ", unwrap_res(res_ok));
+	println("opt_some has a value of ", expect_opt(opt_none, "Msg"));
+
 }
